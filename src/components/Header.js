e88d205999066829471dd8c16511e79f031e6df9
akshpesa <122724899+akshpesa@users.noreply.github.com>
2024-07-10 00:12:13 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
+// Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-function Header() {
+const Header = () => {
   return (
     <header className="header">
-      <h1>Shopify</h1>
       <nav>
+        <h1>Shopify</h1>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/shop">Shop</Link></li>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/register">Register</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
         </ul>
       </nav>
     </header>
